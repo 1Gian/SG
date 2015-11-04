b9ddc9f85f520ed1b0a7d4b4d9b2a975dccd242b
1Gian <giansemi@gmail.com>
2015-11-04 05:54:19 +0300

    changes

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -172,7 +172,30 @@ $('.card').click(function() {
 
 /** TRIAL CARDS PART 2B **/
 
-$('.flip').click(function(e){
-  $(this).find('.card').toggleClass('flipped');
-  e.preventDefault();
+$(document).ready(function() {
+  $('#qrIos').qrcode({
+    width: 200,
+    height: 200,
+    text: "iOS_URL_HERE"
+  });
+  $('#qrAndroid').qrcode({
+    width: 200,
+    height: 200,
+    text: "DROID_URL_HERE"
+  });
+  $('#qrWeb').qrcode({
+    width: 200,
+    height: 200,
+    text: "WINDOWS_URL_HERE"
+  });
 });
+
+function rotateCard(btn) {
+  var $card = $(btn).closest('.card-container');
+  console.log($card);
+  if ($card.hasClass('hover')) {
+    $card.removeClass('hover');
+  } else {
+    $card.addClass('hover');
+  }
+}
